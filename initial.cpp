0324e6d0394bbd84a8567fcb86038cb3a4c8b053
SushamMoulaChoudhury <100505804+SushamMoulaChoudhury@users.noreply.github.com>
2022-04-14 21:17:58 +0600

    Added main function

--- initial.cpp
+++ initial.cpp
@@ -1,5 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
-{
+int main(){
+  int h=0, m=0, s=0;
+  
+    while (s>=0){
+      cout<<h<<":"<<m<<":"<<s<<endl;
+      s++;
+      if(s==60){
+        s=0;
+        m++;
+        }
+      if(m==60){
+        m=0;
+        h++;
+        }
+      }
 }
